3e9d63199f777472024a20ddb2e4375ed5c63c1f
Casadina <casadina@tuta.io>
2021-11-29 21:31:30 +0800

    Quick learn List and spellchecker

--- WordScramble/ContentView.swift
+++ WordScramble/ContentView.swift
@@ -9,8 +9,31 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        let input = "a b c"
+        let letters = input.components(separatedBy: " ")
+        let letter = letters.randomElement()
+        let trimmed = letter?.description.trimmingCharacters(in: .whitespacesAndNewlines)
+        let word = "swift"
+        let checker = UITextChecker()
+        let range = NSRange(location: 0, length: word.utf16.count)
+        let misspelledRange = checker.rangeOfMisspelledWord(in: word, range: range, startingAt: 0, wrap: false, language: "en")
+        let allGood = misspelledRange.location == NSNotFound
+        HStack {
+            List {
+                Section(trimmed ?? "o") {
+                    ForEach(0..<5) {
+                        Text("Dynamic row \($0)")
+                    }
+                }
+                Section("Section 2") {
+                    Text("Static Row 1")
+                }
+            }
+            .listStyle(.inset)
+            Form {
+                Text("Hello")
+            }
+        }
     }
 }
 
